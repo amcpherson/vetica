9bb179963b229f7090a6dc117af10fbd252648b1
Andrew McPherson <andrew.mcpherson@gmail.com>
2020-09-19 15:38:58 -0400

    updates

--- setup.py
+++ setup.py
@@ -3,7 +3,5 @@ from setuptools import setup, find_packages
 setup(
     name='vetica',
     packages=find_packages(),
-    version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
     package_data={'vetica': ['data/*.ttf']},
 )
